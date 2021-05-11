8f11f4b479baa520e7d49975311f0ffe3a4ef626
Jlakku <Skoch116@yandex.ru>
2021-05-11 18:28:57 +0300

    BMI-Calculator

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/artem/developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/artem/AndroidStudioProjects/flutter_app/BMI-Calculator-Flutter-Completed"
+export "FLUTTER_TARGET=/Users/artem/AndroidStudioProjects/flutter_app/BMI-Calculator-Flutter-Completed/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/artem/AndroidStudioProjects/flutter_app/BMI-Calculator-Flutter-Completed/.dart_tool/package_config.json"
